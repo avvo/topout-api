4782a27fee39996b252998696196878b965edd4e
Carson Stauffer <carsonstauffer@gmail.com>
2017-03-29 14:40:36 -0700

    Create github_scraper app

---
+++ apps/github_scraper/lib/github_scraper.ex
@@ -0,0 +1,18 @@
+defmodule GithubScraper do
+  @moduledoc """
+  Documentation for GithubScraper.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GithubScraper.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end

c934324a531e0b9546dcb2679b89dcd7bc3ba2d9
Carson Stauffer <carsonstauffer@gmail.com>
2017-03-29 16:12:38 -0700

    Send HTTP request to GitHub; Use GitHub access token

--- apps/github_scraper/lib/github_scraper.ex
+++ apps/github_scraper/lib/github_scraper.ex
@@ -1,23 +1,21 @@
 defmodule GithubScraper do
-  @moduledoc """
-  Documentation for GithubScraper.
-  """
 
-  @doc """
-  Hello world.
+  alias HTTPoison, as: Http
+  alias Poison, as: Json
 
-  ## Examples
+  use Http.Base
 
-      iex> GithubScraper.hello
-      :world
+  @endpoint "https://api.github.com/graphql"
+  @testquery '{"query": "{ viewer { login name } }"}'
 
-  """
-  def hello do
-    :world
-  end
+  @body @testquery
+  @headers [
+    "Authorization": "Bearer #{Application.get_env(:ex_github, :access_token)}"
+  ]
 
+  # WARNING: you must have an environment variable named GITHUB_ACCESS_TOKEN defined which contains
+  #          a valid github access token or this will fail
   def scrape do
-    IO.puts "Success!"
-    :ok
+    {:ok, response} = post(@endpoint, @body, @headers)
   end
 end

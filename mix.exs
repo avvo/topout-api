1354276c96108808586c2d5b80253fc00e27de00
Steve Wagner <swagner@avvo.com>
2017-03-29 10:14:51 -0700

    Adding mix docker as dependency

--- mix.exs
+++ mix.exs
@@ -21,6 +21,8 @@ defmodule TopoutApi.Mixfile do
   # Dependencies listed here are available only for this project
   # and cannot be accessed from applications inside the apps folder
   defp deps do
-    []
+    [
+	{ :mix_docker, "~> 0.3.0" }
+    ]
   end
 end

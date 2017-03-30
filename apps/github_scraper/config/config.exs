e6a6ce0a3b91a55811caabeda98c1b5283052c58
Steve Wagner <swagner@avvo.com>
2017-03-30 13:37:54 -0700

    Flailing

--- apps/github_scraper/config/config.exs
+++ apps/github_scraper/config/config.exs
@@ -5,6 +5,6 @@ config :quantum, :github_scraper,
     "* * * * *": &GithubScraper.scrape/0
   ]
 
-config :ex_github, access_token: System.get_env("GITHUB_ACCESS_TOKEN")
+config :ex_github, access_token: "THIS_IS_MY_TOKEN" ## System.get_env("GITHUB_ACCESS_TOKEN")
 
 import_config "#{Mix.env}.exs"

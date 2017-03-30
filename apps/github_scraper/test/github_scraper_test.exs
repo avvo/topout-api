d185abb1f39e87d1c57eb28930816d01bcd16404
John Fearnside <jfearnside@avvo.com>
2017-03-30 11:30:45 -0700

    Exclude external tests by default

--- apps/github_scraper/test/github_scraper_test.exs
+++ apps/github_scraper/test/github_scraper_test.exs
@@ -2,6 +2,7 @@ defmodule GithubScraperTest do
   use ExUnit.Case
   doctest GithubScraper
 
+  @tag :external
   describe "scrape" do
     test "returns results successfully" do
       assert :ok == GithubScraper.scrape

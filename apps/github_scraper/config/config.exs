1e1a071592603b5337c9a137cf99ca32659e8df1
Steve Wagner <swagner@avvo.com>
2017-03-30 13:45:11 -0700

    Removing the function reference, because that's what RTFM revealed.

--- apps/github_scraper/config/config.exs
+++ apps/github_scraper/config/config.exs
@@ -2,9 +2,9 @@ use Mix.Config
 
 config :quantum, :github_scraper,
   cron: [
-    "* * * * *": &GithubScraper.scrape/0
+    "* * * * *": {"GithubScraper", :scrape}
   ]
 
-config :ex_github, access_token: "THIS_IS_MY_TOKEN" ## System.get_env("GITHUB_ACCESS_TOKEN")
+config :ex_github, access_token: System.get_env("GITHUB_ACCESS_TOKEN")
 
 import_config "#{Mix.env}.exs"

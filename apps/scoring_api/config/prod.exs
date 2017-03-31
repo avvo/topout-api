a5c0aaf34796966191a6c059ea6377cf20d989cd
Carson Stauffer <carsonstauffer@gmail.com>
2017-03-30 19:04:14 -0700

    Remove phoenix manifest caching

--- apps/scoring_api/config/prod.exs
+++ apps/scoring_api/config/prod.exs
@@ -14,7 +14,6 @@ use Mix.Config
 config :scoring_api, ScoringApi.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info

69400eb553f82d45f252cb01bd80e4588b3f9ce8
Matt <mpurdy@avvo.com>
2017-03-30 17:32:26 -0700

    Make the summary api summarize

--- apps/scoring_api/test/controllers/leader_board_summary_controller_test.exs
+++ apps/scoring_api/test/controllers/leader_board_summary_controller_test.exs
@@ -1,5 +1,6 @@
 defmodule ScoringApi.LeaderBoardSummaryControllerTest do
   use ScoringApi.ConnCase
+  alias ScoringApi.GithubCommit
 
   alias ScoringApi.LeaderBoardSummary
   @valid_attrs %{count: 42, display_name: "some content"}
@@ -10,6 +11,15 @@ defmodule ScoringApi.LeaderBoardSummaryControllerTest do
   end
 
   test "lists all entries on index", %{conn: conn} do
+    valid_attrs = %{
+      commit_id: "test_content7",
+      display_name: "some content",
+      email: "some content",
+      github_id: "some content",
+      repo: "some content"
+    }
+    GithubCommit.changeset(%GithubCommit{}, valid_attrs) |> Repo.insert!
+
     conn = get conn, leader_board_summary_path(conn, :index)
     assert json_response(conn, 200)["data"] != []
   end

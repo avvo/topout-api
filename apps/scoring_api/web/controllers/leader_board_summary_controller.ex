9de976a5f3f956c03cbb0aa38edf2530c8b166af
Carson Stauffer <carsonstauffer@gmail.com>
2017-03-30 21:05:27 -0700

    Order score summary by count

--- apps/scoring_api/web/controllers/leader_board_summary_controller.ex
+++ apps/scoring_api/web/controllers/leader_board_summary_controller.ex
@@ -10,7 +10,9 @@ defmodule ScoringApi.LeaderBoardSummaryController do
     leader_board_summary = ScoringApi.Repo.all(
       from c in ScoringApi.GithubCommit, 
         group_by: [:github_id, :display_name], 
-        select: {c.display_name, c.github_id, count(c.id)})
+        select: {c.display_name, c.github_id, count(c.id)},
+        order_by: [desc: :count]
+        )
       |> Enum.map(fn({display_name, id, score}) -> %{id: id, display_name: display_name, score: score} end)
 
     render(conn, "index.json", leader_board_summary: leader_board_summary)
